2330bf6b3430271a516b4859b96910319f9ea26f
Jenny Dog <jenny.jam.internista@gmail.com>
2023-09-22 16:24:08 -0400

    added files for chip logic

---
+++ chip.py
@@ -0,0 +1,12 @@
+from amaranth import *
+
+class MIPSAdder(Elaboratable):
+    """
+    Mips Chip Adder
+    """
+
+    def __init__(self):
+        pass
+
+    def elaborate(self, platform):
+        pass
