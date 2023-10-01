c5449889f87758a819ab213bbba181fd913d008f
Jenny Dog <jenny.jam.internista@gmail.com>
2023-10-01 18:57:55 -0400

    adding machinery for Decoder module

--- mips/cpu/decoder.py
+++ mips/cpu/decoder.py
@@ -2,7 +2,25 @@ from amaranth import *
 import isa
 
 class Decoder(Elaboratable):
+    """
+    Decoder module that parses a value in and passes all values 
+    out.
+
+    Any non-set value are going to default to 0's (this is moreso
+    a quirk of Amaranth).
+
+    Attributes:
+        inst: input instruction value
+        opcode: output opcode value
+        rs: output rs register value
+        rt: output rt register value
+        rd: output rd register value
+        func: output func value
+        imm: immediate value
+        addr: address value
+    """
     def __init__(self):
+        self.inst = Signal(32)
         self.opcode = Signal(unsigned(6))
         self.rs: Signal(unsigned(5))
         self.rt = Signal(unsigned(5))
@@ -13,8 +31,8 @@ class Decoder(Elaboratable):
         self.addr = Signal(26)
         pass
 
-    def elaborate(self, platform):
+    def elaborate(self, platform) -> Module:
         m = Module()
-
-
+        inst = Signal(isa.Inst)
+        
         return m
