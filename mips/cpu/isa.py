d20081d07851182372d4352f2a650e854af58df3
Jenny Dog <jenny.jam.internista@gmail.com>
2023-10-01 17:23:32 -0400

    Add some of the logic for the code structure of decoding

--- mips/cpu/isa.py
+++ mips/cpu/isa.py
@@ -1,36 +1,46 @@
+from amaranth import *
 from amaranth.lib import enum
+from amaranth.lib import data
+
 
 class Funct(enum.Enum, shape=6):
     """
     Binary encoding of instructions with 0b00000 opcode
     """
-    ## TODO: Check the binary encoding is right?
-    # Add
     ADD = 0b100000
     "trapping add"
     ADDU = 0b100001
     "non-trapping add"
-    # div/mul: not implemented
-
     SLL = 0b000000
-    SLLV = 0b000100
-
     SRA = 0b000011
-    SRAV = 0b000111
-
     SRL = 0b000010
-    SRLV = 0b000110
-
-    # AND
     AND = 0b100100
     "And"
-    
     NOR = 0b100111
     OR = 0B100101
-
     SUB = 0b100010
     SUBU = 0b100011
-
     XOR = 0b100110
 
+class Opcode(enum.Enum, shape=6):
+    pass
 
+class Instr(data.Struct):
+    opcode: Opcode
+    layout: data.UnionLayout({
+        "reg": data.StructLayout({
+            "rs": unsigned(5),
+            "rt": unsigned(5),
+            "rd": unsigned(5),
+            "a": unsigned(5),
+            "funct": Funct
+            }),
+        "imm": data.StructLayout({
+            "rs": unsigned(5),
+            "rt": unsigned(5),
+            "imm": unsigned(16)
+        }),
+        "jmp": data.StructLayout({
+            "imm": unsigned(26)
+        })
+    })
